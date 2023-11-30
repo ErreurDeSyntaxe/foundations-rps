9aebb49d557048ee363bab8a20b8339050ab8333
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2023-11-30 18:20:00 +0800

    Add the final touch
    The program can now play until a player has five points and start
    a new game if the user wants. I meets all objectives, or so I believe
    right now. I will check tomorrow to make sure, but it seems to me
    like this is ready to ship. How many programmers said that and
    regretted it almost immediately, I wonder.

--- js/script.js
+++ js/script.js
@@ -44,7 +44,7 @@ function validateChoice(choice) {
 //This plays RPS with the user until there is a winner
 //If the player and computer tie, the round is not over
 function playRound (computer, human) {
-    console.log(computer + " " + human);
+    //console.log(computer + " " + human);
     if (computer == human) {
         console.log("It's a tie! Keep playing!");
         return undefined;
@@ -55,9 +55,76 @@ function playRound (computer, human) {
     }
 }
 
-//
-let roundWinner = undefined;
-while (roundWinner === undefined) {
-    roundWinner = playRound(getComputerChoice(), getPlayerChoice());
+//This code plays a round and finds a winner
+
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+    
+    while (computerScore < 5 && humanScore < 5) {
+        let roundWinner = undefined;
+        while (roundWinner === undefined) {
+            let computer = getComputerChoice();
+            let human = getPlayerChoice();
+            roundWinner = playRound(computer, human);
+            if (roundWinner == "human") {
+                let sentence = "You win this round! " + hand[human][0].toUpperCase() + hand[human].slice(1) + " beats " + hand[computer] + "!";
+                //The difference here is just to practice backticks and quotation marks
+                console.log(sentence);
+                humanScore++;
+            } else if (roundWinner == "computer") {
+                //The difference here is just to practice backticks and quotation marks
+                console.log(`You lose this round! ${hand[computer][0].toUpperCase()}${hand[computer].slice(1)} beats ${hand[human]}!`);
+                computerScore++;
+            }
+        }
+    }
+    if (humanScore == 5) {
+        endGame("human", computerScore, humanScore);
+    } else {
+        endGame("computer", computerScore, humanScore);
+    }
 }
-console.log(roundWinner);
+
+//This function resets the score of both players in case
+//the user wants to play again
+function resetScore() {
+    humanScore = 0;
+    computerScore = 0;
+}
+
+//This function checks if the user wants to play again
+//It doesn't force the user to play again
+function playAgain() {
+    let answer = "place holder value";
+    
+    //asks until the answer is either 'y' or 'n'
+    //if the user hits the cancel button from the 
+    //prompt box, the value of answer becomes null
+    while (answer != null && answer != "y" && answer != "n") {
+        answer = prompt("Would you like to play again? Enter 'y' for yes or 'n' for no.");
+        if (answer == "y") {
+            console.clear();
+            playGame();
+        } else if (answer == "n") {
+            console.log("Have a good one!");
+            return;
+        }
+    }
+}
+
+//This function displays a result message (victory or defeat)
+//It calls resetScore() and playAgain() to check if the user wants to play
+function endGame(winner, computerScore, humanScore) {
+    if (winner == "human") {
+        console.log(`You have won the match ${humanScore} to ${computerScore}! Congratulations!! Cntl + R to play again.`);
+        resetScore();
+        playAgain();
+    } else {
+        console.log(`You have lost the match ${computerScore} to ${humanScore}. Better luck next time! Cntl + R to play again.`);
+        resetScore();
+        playAgain();
+    }
+}
+
+playGame();
