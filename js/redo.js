e9d7e8133b7dfc18356d1cd9b898c5ae1b72a491
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2024-05-28 14:03:31 +0800

    Implement button play

--- js/redo.js
+++ js/redo.js
@@ -4,6 +4,7 @@ const HAND = ['rock', 'paper', 'scissors'];
 const scores = [0, 0];
 const humanScore = document.querySelector('.p1-score');
 const computerScore = document.querySelector('.p2-score');
+const playButtons = document.querySelectorAll('.btn--play');
 
 const getComputerChoice = function () {
   return HAND[Math.floor(Math.random() * 3)];
@@ -21,13 +22,23 @@ const processInput = function (userInput) {
   return false;
 };
 
+// Used in the no-GUI version
 const getHumanChoice = function () {
   let humanChoice;
   do {
-    humanChoice = prompt('Rock, Paper, Scissors! What say you?');
+    // humanChoice = prompt('Rock, Paper, Scissors! What say you?');
+    humanChoice = getComputerChoice();
     humanChoice = processInput(humanChoice);
   } while (!humanChoice);
   return humanChoice;
+};
+
+const displayHands = function (humanHand, computerHand) {
+  log(`*****\nHuman: ${humanHand} \nComputer: ${computerHand}`);
+};
+
+const displayScores = function () {
+  log(`Human: ${scores[0]}\nComputer: ${scores[1]}\n*****`);
 };
 
 const determineWinner = function (humanHand, computerHand) {
@@ -57,19 +68,25 @@ const determineWinner = function (humanHand, computerHand) {
       scores[1]++;
     }
   }
+  displayHands(humanHand, computerHand);
   humanScore.textContent = scores[0];
   computerScore.textContent = scores[1];
-  log(scores);
+  displayScores();
+  checkGameOver();
 };
 
 const checkGameOver = function () {
   if (scores[0] === 5 || scores[1] === 5) {
-    log(`Game Over! ${scores[0] === 5 ? 'Player 1' : 'Player 2'} wins!`);
-    return true;
+    log(
+      `Game Over! ${
+        scores[0] === 5 ? 'The Resourceful Human' : 'The Almighty Computer'
+      } wins!`
+    );
+    playButtons.forEach((button) => (button.disabled = true));
   }
-  return false;
 };
 
+// Used in the no-GUI version
 const playRound = function () {
   log('********************');
   const humanChoice = getHumanChoice();
@@ -79,6 +96,7 @@ const playRound = function () {
   determineWinner(humanChoice, computerChoice);
 };
 
+// Used in the no-GUI version
 const startGame = function () {
   do {
     playRound();
@@ -88,5 +106,15 @@ const startGame = function () {
 };
 
 window.addEventListener('load', () => {
-  startGame();
+  playButtons[0].addEventListener('click', () => {
+    determineWinner('rock', getComputerChoice());
+  });
+  playButtons[1].addEventListener('click', () => {
+    determineWinner('paper', getComputerChoice());
+  });
+  playButtons[2].addEventListener('click', () => {
+    determineWinner('scissors', getComputerChoice());
+  });
+
+  // startGame();
 });
